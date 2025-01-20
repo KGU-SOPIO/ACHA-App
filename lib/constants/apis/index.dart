d3c2699cf015aea8117a7bc34d0f7ea700a2e4f0
준서 <junseokr@icloud.com>
2025-01-20 21:15:20 +0900

    FEAT : FCM APNs

--- lib/constants/apis/index.dart
+++ lib/constants/apis/index.dart
@@ -6,7 +6,7 @@ export 'package:acha/constants/apis/today_course.dart';
 export 'package:acha/constants/apis/alert.dart';
 
 class BaseApiEndpoints {
-  static const String baseUrl = 'http://10.0.2.2:8000';
+  static const String baseUrl = 'http://127.0.0.1:8000';
   /// DEBUG
   /// iOS: 127.0.0.1
   /// Android: 10.0.2.2
